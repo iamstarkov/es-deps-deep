1f056622c989977406745dd6a54c10e55e6b9703
Vladimir Starkov <iamstarkov@gmail.com>
2016-04-28 01:40:36 +0200

    implement thing

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-function esDepsDeepAsync(input) {
-  if (typeof input !== 'string') {
-    return Promise.reject(new TypeError(`\input\` should be \`String\`, got \`${typeof input}\``));
-  }
-  return Promise.resolve(input);
+import R from 'ramda';
+import Promise from 'pinkie-promise';
+import binded from 'binded';
+import esDepsResolved from 'es-deps-resolved';
+import resolveCwd from 'resolve-cwd';
+import { esDepUnit } from 'es-dep-unit';
+import contract from 'neat-contract';
+
+const { resolve, all, reject } = binded(Promise);
+
+// esDepsDeep :: String -> Array[Object]
+function esDepsDeep(file, excludeFn = R.F) {
+  let cache = [];
+
+  const deps = R.pipeP(resolve,
+    R.prop('resolved'),
+    R.ifElse(R.isNil, R.always([]), esDepsResolved));
+
+  const walk = item => {
+    if (R.contains(item.resolved, cache)) {
+      return resolve([]);
+    } else {
+      cache.push(item.resolved);
+      return R.pipeP(resolve,
+        deps,
+        mapWalk,
+        R.unnest,
+        R.reject(excludeFn),
+        R.prepend(item)
+      )(item);
+    }
+  };
+
+  const mapWalk = items => all(items.map(walk));
+
+  return R.pipeP(resolve,
+    contract('file', String),
+    resolveCwd,
+    R.when(R.isNil, () => reject(new Error(`Can't find and open \`${file}\``))),
+    esDepUnit(null, null),
+    R.of,
+    R.reject(excludeFn),
+    mapWalk,
+    R.unnest
+  )(file);
 }
 
-function esDepsDeep(input) {
-  if (typeof input !== 'string') {
-    throw new TypeError(`\input\` should be \`String\`, got \`${typeof input}\``);
-  }
-  return input;
-}
-
-export { esDepsDeep, esDepsDeepAsync };
+export default esDepsDeep;
