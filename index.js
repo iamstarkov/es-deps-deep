66c05ac52e0f7c7a51d182bce57ca9ee0ace842b
Vladimir Starkov <iamstarkov@gmail.com>
2016-04-28 12:20:00 +0200

    simplify

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle, no-use-before-define */
 import R from 'ramda';
 import Promise from 'pinkie-promise';
 import binded from 'binded';
@@ -6,34 +7,33 @@ import resolveCwd from 'resolve-cwd';
 import { esDepUnit } from 'es-dep-unit';
 import contract from 'neat-contract';
 
-const { resolve, all, reject } = binded(Promise);
+const { resolve: toPromise, all, reject } = binded(Promise);
+
+const _resolved = R.prop('resolved');
 
 // esDepsDeep :: String -> Array[Object]
 function esDepsDeep(file, excludeFn = R.F) {
-  let cache = [];
+  var cache = []; // eslint-disable-line
 
-  const deps = R.pipeP(resolve,
-    R.prop('resolved'),
-    R.ifElse(R.isNil, R.always([]), esDepsResolved));
+  const deps = R.pipeP(toPromise,
+    _resolved,
+    R.ifElse(R.isNil, R.always([]), esDepsResolved)
+  );
 
-  const walk = item => {
-    if (R.contains(item.resolved, cache)) {
-      return resolve([]);
-    } else {
-      cache.push(item.resolved);
-      return R.pipeP(resolve,
-        deps,
-        mapWalk,
-        R.unnest,
-        R.reject(excludeFn),
-        R.prepend(item)
-      )(item);
-    }
-  };
+  const isInCache = R.pipe(_resolved, R.contains(R.__, cache));
 
-  const mapWalk = items => all(items.map(walk));
+  const walk = item => R.ifElse(isInCache, R.always([]), R.pipeP(toPromise,
+    R.tap(_ => { cache.push(_.resolved); }),
+    deps,
+    mapWalk,
+    R.unnest,
+    R.reject(excludeFn),
+    R.prepend(item)
+  ))(item);
 
-  return R.pipeP(resolve,
+  const mapWalk = R.pipeP(toPromise, R.map(walk), all);
+
+  return R.pipeP(toPromise,
     contract('file', String),
     resolveCwd,
     R.when(R.isNil, () => reject(new Error(`Can't find and open \`${file}\``))),
